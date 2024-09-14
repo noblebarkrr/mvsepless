378cc9d0882eb1cf83767263240f4325e84d1496
noblebarkrr <noble.barker52@gmail.com>
2024-09-14 16:23:43 +0300

    Add files via upload

--- clear.sh
+++ clear.sh
@@ -1,5 +1,4 @@
-rm -rf /tmp/gradio/
 rm -rf /content/temp/
-rm -rf /content/input/
+rm -rf /content/inputsep/
 rm instrumental.wav vocals.wav backvocals.wav leadvocals.wav fullvocals.wav
-cd /content/ && mkdir temp temp/backvocals temp/vocals input
+cd /content/ && mkdir temp temp/backvocals temp/vocals inputsep
