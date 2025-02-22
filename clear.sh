d1f6f2dea84999315b07972ee78fccf8a5db8b73
noblebarkrr <noble.barker52@gmail.com>
2025-02-22 13:16:17 +0300

    Delete clear.sh

--- clear.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /content/temp/
-rm -rf /content/inputsep/
-rm instrumental.wav vocals.wav backvocals.wav leadvocals.wav fullvocals.wav
-cd /content/ && mkdir temp temp/backvocals temp/vocals inputsep
