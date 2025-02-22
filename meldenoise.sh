f75d96622ffce7b8f4f9746c7f7a2f8799fa41e0
noblebarkrr <noble.barker52@gmail.com>
2025-02-22 13:16:32 +0300

    Delete meldenoise.sh

--- meldenoise.sh
+++
@@ -1,3 +0,0 @@
-cd /content/Mel-Band-Roformer-Vocal-Model && python inference.py --config_path /content/Mel-Band-Roformer-Vocal-Model/configs/config_vocals_mel_band_roformer.yaml --model_path /content/melmodels/denoise_mel_band_roformer_aufr33_sdr_27.9959.ckpt --input_folder /content/inputsep --store_dir /content/temp/
-
-bash output.sh
