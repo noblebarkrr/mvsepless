63999887ca5256d942e2f0c5a7511befb0d7ae0e
noblebarkrr <noble.barker52@gmail.com>
2025-03-08 01:24:09 +0300

    experimental

---
+++ code_infer.py
@@ -0,0 +1,73 @@
+import os
+import argparse
+import subprocess
+from inference import mvsep_offline
+from config import conf_editor
+
+
+def code_infer():
+    parser = argparse.ArgumentParser(description="Обработка входных и выходных директорий и кода модели.")
+    parser.add_argument("-i", "--input", type=str, help="folder with mixtures to process")
+    parser.add_argument("-o", "--output", default="", type=str, help="path to store results")
+    parser.add_argument("-inst", "--instrum", action='store_true', help="invert vocals")
+    parser.add_argument("-mcode", "-mc", "--modelcode", dest='modelcode', type=int, required=True, help="Код модели")
+    parser.add_argument("-of", "--output_format", type=str, choices=['mp3', 'wav', 'flac'], default='wav', help="Format of output files")
+    parser.add_argument("-tta", "--use_tta", action='store_true', help="Flag adds test time augmentation during inference (polarity and channel inverse). While this triples the runtime, it reduces noise and slightly improves prediction quality.")
+    args = parser.parse_args()
+
+    model_dir = f"model/{args.modelcode}"
+    os.makedirs(model_dir, exist_ok=True)
+    
+    from models_list import get_model_config
+    config = get_model_config(args.modelcode)
+
+    if config:
+        model_name = config["model_name"]
+        arch = config["arch"]
+        ckpt_url = config["ckpt_url"]
+        conf_url = config["conf_url"]
+
+        print(f"Model Name: {model_name}")
+        print(f"Architecture: {arch}")
+        print(f"Checkpoint URL: {ckpt_url}")
+        print(f"Config URL: {conf_url}")
+    else:
+        print(f"Model with code {model_code} not found.")
+
+    if arch == "mel_band_roformer" or arch == "bs_roformer" or arch == "mdx23c" or arch == "scnet":
+        conf = f"{model_dir}/config.yaml"
+        ckpt = f"{model_dir}/model.ckpt"
+        infer = "mss"
+
+    elif arch == "medleyvox":
+        conf = f"{model_dir}/vocals.json"
+        ckpt = f"{model_dir}/vocals.pth"
+        infer = "medley_vox"
+
+    for local_path, url_model in [(ckpt, ckpt_url), (conf, conf_url)]:
+        download_ckpt = ("wget", "-O", str(local_path), str(url_model))
+        subprocess.run(download_ckpt, check=True)
+
+    if infer == "mss":
+        conf_editor(conf)
+        output_format = args.output_format
+        mvsep_offline(
+            args.input,
+            args.output,
+            arch,
+            conf,
+            ckpt,
+            0,
+            args.instrum,
+            False,
+            args.output_format,
+            args.use_tta,
+            False, False)
+    elif infer == "medley_vox":
+        # coming_soon m_vox(input, output, args.modelcode, args.output_format)
+        print(infer)
+
+        
+
+if __name__ == "__main__":
+    code_infer()
---
+++ inference.py
@@ -0,0 +1,179 @@
+import argparse
+import time
+import librosa
+from tqdm.auto import tqdm
+import sys
+import os
+import glob
+import torch
+import numpy as np
+import soundfile as sf
+import torch.nn as nn
+from utils import prefer_target_instrument, demix, get_model_from_config
+
+def run_inference(model, config, input_folder, store_dir, device, model_type, extract_instrumental, disable_detailed_pbar, output_format, use_tta, verbose):
+    start_time = time.time()
+    model.eval()
+    all_mixtures_path = glob.glob(input_folder + '/*.*')
+    all_mixtures_path.sort()
+    sample_rate = 44100
+    if 'sample_rate' in config.audio:
+        sample_rate = config.audio['sample_rate']
+    print('Total files found: {} Use sample rate: {}'.format(len(all_mixtures_path), sample_rate))
+
+    instruments = prefer_target_instrument(config)
+
+    os.makedirs(store_dir, exist_ok=True)
+
+    if not verbose:
+        all_mixtures_path = tqdm(all_mixtures_path, desc="Total progress")
+
+    if disable_detailed_pbar:
+        detailed_pbar = False
+    else:
+        detailed_pbar = True
+
+    for path in all_mixtures_path:
+        print("Starting processing track: ", path)
+        if not verbose:
+            all_mixtures_path.set_postfix({'track': os.path.basename(path)})
+        try:
+            mix, sr = librosa.load(path, sr=sample_rate, mono=False)
+        except Exception as e:
+            print('Cannot read track: {}'.format(path))
+            print('Error message: {}'.format(str(e)))
+            continue
+
+        # Convert mono to stereo if needed
+        if len(mix.shape) == 1:
+            mix = np.stack([mix, mix], axis=0)
+
+        mix_orig = mix.copy()
+        if 'normalize' in config.inference:
+            if config.inference['normalize'] is True:
+                mono = mix.mean(0)
+                mean = mono.mean()
+                std = mono.std()
+                mix = (mix - mean) / std
+
+        if use_tta:
+            # orig, channel inverse, polarity inverse
+            track_proc_list = [mix.copy(), mix[::-1].copy(), -1. * mix.copy()]
+        else:
+            track_proc_list = [mix.copy()]
+
+        full_result = []
+        for mix in track_proc_list:
+            waveforms = demix(config, model, mix, device, pbar=detailed_pbar, model_type=model_type)
+            full_result.append(waveforms)
+
+        # Average all values in single dict
+        waveforms = full_result[0]
+        for i in range(1, len(full_result)):
+            d = full_result[i]
+            for el in d:
+                if i == 2:
+                    waveforms[el] += -1.0 * d[el]
+                elif i == 1:
+                    waveforms[el] += d[el][::-1].copy()
+                else:
+                    waveforms[el] += d[el]
+        for el in waveforms:
+            waveforms[el] = waveforms[el] / len(full_result)
+
+        # Create a new `instr` in instruments list, 'instrumental' 
+        if extract_instrumental and config.training.target_instrument is not None:
+            # Create a list of instruments excluding the target instrument
+            second_stem = [s for s in config.training.instruments if s != config.training.target_instrument]
+            
+            # Choose a single instrument from second_stem
+            if second_stem:
+                # If there are elements in second_stem, use the first one (or handle according to your logic)
+                second_stem_key = second_stem[0]  
+                
+                if second_stem_key not in instruments:
+                    instruments.append(second_stem_key)
+                
+                # Output "instrumental", which is an inverse of 'vocals' or the first stem in list if 'vocals' absent
+                waveforms[second_stem_key] = mix_orig - waveforms[instruments[0]]
+
+        for instr in instruments:
+            estimates = waveforms[instr].T
+            if 'normalize' in config.inference:
+                if config.inference['normalize'] is True:
+                    estimates = estimates * std + mean
+            file_name, _ = os.path.splitext(os.path.basename(path))
+            custom_name = f"{file_name}_mt-{model_type}_{instr}"
+            if output_format == "flac":
+                output_file = os.path.join(store_dir, f"{custom_name}.flac")
+                sf.write(output_file, estimates, sr, subtype='PCM_16')
+            elif output_format == "mp3":
+                from pydub import AudioSegment
+                temp_wav_dir = "/tmp/wav_msst"
+                os.makedirs(temp_wav_dir, exist_ok=True)
+                output_file = os.path.join(store_dir, f"{custom_name}.mp3")
+                temp_wav = os.path.join(temp_wav_dir, f"{custom_name}.wav")
+                sf.write(temp_wav, estimates, sr, subtype='PCM_16')  # Временный WAV
+        
+                # Конвертация в MP3
+                audio = AudioSegment.from_wav(temp_wav)
+                audio.export(output_file, format="mp3", bitrate="320k")
+                os.remove(temp_wav)  # Удаление временного файла
+            elif output_format == "wav":
+                output_file = os.path.join(store_dir, f"{custom_name}.wav")
+                sf.write(output_file, estimates, sr, subtype='PCM_16')
+
+    time.sleep(1)
+    print("Elapsed time: {:.2f} sec".format(time.time() - start_time))
+
+
+def load_model(model_type, config_path, start_check_point, device_ids, force_cpu=False):
+    device = "cpu"
+    if force_cpu:
+        device = "cpu"
+    elif torch.cuda.is_available():
+        print('CUDA is available, use --force_cpu to disable it.')
+        device = "cuda"
+        device = f'cuda:{device_ids[0]}' if type(device_ids) == list else f'cuda:{device_ids}'
+    elif torch.backends.mps.is_available():
+        device = "mps"
+
+    print("Using device: ", device)
+
+    model_load_start_time = time.time()
+    torch.backends.cudnn.benchmark = True
+
+    model, config = get_model_from_config(model_type, config_path)
+    if start_check_point != '':
+        print('Start from checkpoint: {}'.format(start_check_point))
+        if model_type in ['htdemucs', 'apollo']:
+            state_dict = torch.load(start_check_point, map_location=device, weights_only=False)
+            # Fix for htdemucs pretrained models
+            if 'state' in state_dict:
+                state_dict = state_dict['state']
+            # Fix for apollo pretrained models
+            if 'state_dict' in state_dict:
+                state_dict = state_dict['state_dict']
+        else:
+            state_dict = torch.load(start_check_point, map_location=device, weights_only=True)
+        model.load_state_dict(state_dict)
+    print("Instruments: {}".format(config.training.instruments))
+
+    # in case multiple CUDA GPUs are used and --device_ids arg is passed
+    if type(device_ids) == list and len(device_ids) > 1 and not force_cpu:
+        model = nn.DataParallel(model, device_ids = device_ids)
+
+    model = model.to(device)
+
+    print("Model load time: {:.2f} sec".format(time.time() - model_load_start_time))
+
+    return model, config, device
+
+
+def mvsep_offline(input_folder, store_dir, model_type, config_path, start_check_point, device_ids, extract_instrumental, disable_detailed_pbar, output_format, use_tta, force_cpu, verbose):
+    model, config, device = load_model(model_type, config_path, start_check_point, device_ids, force_cpu)
+    run_inference(model, config, input_folder, store_dir, device, model_type, extract_instrumental, disable_detailed_pbar, output_format, use_tta, verbose)
+
+
+if __name__ == "__main__":
+    mvsep_offline(input_folder="path/to/input/folder", store_dir="path/to/store/dir")
