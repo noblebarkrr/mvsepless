96e2709ca47035e25d61614228f895fb64a90428
noblebarkrr <noble.barker52@gmail.com>
2026-01-25 15:13:56 +0300

    Add files via upload

--- mvsepless/infer_utils.py
+++ mvsepless/infer_utils.py
@@ -67,6 +67,9 @@ def get_model_from_config(model_type: str, config_path: str) -> Tuple:
         elif hasattr(config, "conditional"):
             from models.bs_roformer import BSRoformer_Conditional
             model = BSRoformer_Conditional(**dict(config.model))
+        elif hasattr(config, "unwa_inst_large_2"):
+            from models.bs_roformer import BSRoformer_2
+            model = BSRoformer_2(**dict(config.model))
         else:
             from models.bs_roformer import BSRoformer
             model = BSRoformer(**dict(config.model))
