9a3b868a01014e8d5208eb2d43cd0acfd1ce74f8
noblebarkrr <noble.barker52@gmail.com>
2026-04-16 01:00:37 +0300

    Update infer_utils.py

--- mvsepless/infer_utils.py
+++ mvsepless/infer_utils.py
@@ -107,7 +107,7 @@ def get_model_from_config(model_type: str, config_path: str) -> Tuple[Any, Any]:
         elif hasattr(config, "unwa_inst_large_2"):
             from models.bs_roformer import BSRoformer_2
             model = BSRoformer_2(**dict(config.model))
-        elif hasattr(self.config, "siamese"):
+        elif hasattr(config, "siamese"):
             from models.bs_roformer import BSSiameseRoformer
             model = BSSiameseRoformer(**dict(config.model))
         else:
